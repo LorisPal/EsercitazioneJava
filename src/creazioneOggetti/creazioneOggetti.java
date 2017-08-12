275dc21f44fbb5b881f2247d681d8833c3c26d33
LorisPalmucci <generalle913@gmail.com>
2017-08-12 17:21:31 +0200

    aggiunta riga char

--- src/creazioneOggetti/creazioneOggetti.java
+++ src/creazioneOggetti/creazioneOggetti.java
@@ -2,10 +2,15 @@ package creazioneOggetti;
 
 public class creazioneOggetti {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		String s=new String("asdf");
+		
+		Character c=new Character('c');
+		String C=c.toString();
+		
 		System.out.println(s);
-
+		System.out.println(C);
 	}
 
 }
